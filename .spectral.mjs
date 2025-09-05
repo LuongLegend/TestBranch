6b4f13d8bb17bb8913a45c3149a5854e54540f65
luongprolv99@gmail.com <luongprolv99@gmail.com>
2025-09-05 15:58:53 +0700

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjQxNTEz/spectral.js?branch_slug=master&token=dd702b27-0906-4370-93fa-a34f88f5289a";
+export default { extends: ruleset };
